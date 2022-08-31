f5feaaf35b139ec62e94a5dfd3617fec12131ba8
Sergii <s.o.petrov@gmail.com>
2022-08-31 12:31:42 +0300

    chngs

--- Lesson_3_HW2/3.py
+++ Lesson_3_HW2/3.py
@@ -1,6 +1,6 @@
 word = str(input("Введить назву:"))
 word2 = word.split("_")
 a, b, c = str(word2[0]), str(word2[1]), str(word2[2])
-NewWord = a.capitalize() + b.capitalize()+c.capitalize()
+NewWord = a.capitalize() + b.capitalize() + c.capitalize()
 print(NewWord)
 
--- Lesson_3_HW2/5.py
+++ Lesson_3_HW2/5.py
@@ -3,15 +3,20 @@ MyNumber = str(input("введить трьохзначне число:"))
 a, b, c = int(MyNumber[0]), int(MyNumber[1]), int(MyNumber[2])
 print(a+b+c)
 
+# using arithmetic operations
+int_num = input("введить трьохзначне число:")
+first_digit = int(int_num)//100
+second_digit = int(int_num) % 100//10
+third_digit = int(int_num) % 10
+print(first_digit + second_digit + third_digit)
 
 
 #  using list
-MyNumber2 = input("введить трьохзначне число:")
-NumList = [int(MyNumber2[0]), int(MyNumber2[1]), int(MyNumber2[2])]
-print(sum(NumList))
+#  MyNumber2 = input("введить трьохзначне число:")
+#  NumList = [int(MyNumber2[0]), int(MyNumber2[1]), int(MyNumber2[2])]
+#  print(sum(NumList))
 
 # or
-NumList = list(map(int, MyNumber))# used map to get list with integers
-print(sum(NumList))
-
+#  NumList = list(map(int, MyNumber))#  used map to get list with integers
+#  print(sum(NumList))
 
