ef425db5d3dd3bf4264074d6d32032a6288731de
Sergii <s.o.petrov@gmail.com>
2022-09-05 00:42:53 +0300

    fixed 1st task

--- Lesson_4_HW3/1.py
+++ Lesson_4_HW3/1.py
@@ -2,9 +2,9 @@ import math
 
 
 def degrees2radians(degrees):  # returns float
+    """Receives degrees and returns radians"""
     radians = degrees * (math.pi/180)
-    return math.cos(radians)
+    return radians
 
 
-print(degrees2radians(60), degrees2radians(45), degrees2radians(40))
-
+print(math.cos(degrees2radians(60)), (math.cos(degrees2radians(45)), (math.cos(degrees2radians(40)))))
