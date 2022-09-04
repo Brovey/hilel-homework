270550a3061f2eaba91375b8a1802bdedd9139f4
Sergii <s.o.petrov@gmail.com>
2022-09-05 00:48:58 +0300

    improved output and style

--- Lesson_4_HW3/2.py
+++ Lesson_4_HW3/2.py
@@ -2,14 +2,17 @@ from math import sqrt
 
 value1 = int(input("Введить значення першого катету:"))
 value2 = int(input("Введить значення другого катету:"))
+txt_square, txt_perim = "Площа трикутника:", "Периметр трикутника:"
 
 
 def triangle_square_and_perimeter(a, b):  # returns 2 values
-    gipot = sqrt(a**2 + b**2)
+    """Receives values of two legs and returns square and perimetr of triangle """
+    gipot = sqrt(a ** 2 + b ** 2)
     perimetr = a + b + gipot
     square = (a*b)/2
     return square, perimetr
 
 
-print(triangle_square_and_perimeter(value1,value2))
+print(f'{txt_square}{triangle_square_and_perimeter(value1, value2)[0]} \n'  # f-strings so good
+      f'{txt_perim}{triangle_square_and_perimeter(value1, value2)[1]}')
 
--- Lesson_4_HW3/7.py
+++ Lesson_4_HW3/7.py
@@ -2,8 +2,13 @@ number = str(input("Введить трьохзначне число: "))
 
 
 def my_sum(number):
+    """ Sum of 3 digits received as str,  using ASCII values"""
 
-    return (ord(number[0]) - ord("0")) + (ord(number[1]) - ord("0")) + (ord(number[2]) - ord("0"))
+    zero_ascii = ord("0")
+    first_digit = ord(number[0]) - zero_ascii
+    second_digit = ord(number[1]) - zero_ascii
+    third_digit = ord(number[2]) - zero_ascii
+    return first_digit + second_digit + third_digit
 
 
 print(my_sum(number))
