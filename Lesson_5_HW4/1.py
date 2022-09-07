8a8121d8c2ad74313b7c47bf3abc674540aaff7a
Sergii <s.o.petrov@gmail.com>
2022-09-07 20:03:25 +0300

    updte 2

--- Lesson_5_HW4/1.py
+++ Lesson_5_HW4/1.py
@@ -1,8 +1,9 @@
 def is_even(number):  # returns boolean value
+    """Return True if number is Even and False if Odd """
     if number % 2 == 0:
         return True
     else:
         return False
 
 
-print(is_even(454545))
+print(is_even(223))
--- Lesson_5_HW4/6.py
+++ Lesson_5_HW4/6.py
@@ -1,8 +1,10 @@
 def fibonachi(number):
     if number <= 1:
-        return number
+        return 0
+    if number == 2:
+        return 1
     else:
         return fibonachi(number - 1) + fibonachi(number - 2)
 
 
-print(fibonachi(20))
+print(fibonachi(2))
