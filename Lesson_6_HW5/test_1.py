9486a63af264ac12f5e676de1a9f892365efa79c
Sergii <s.o.petrov@gmail.com>
2022-09-13 14:04:49 +0300

    hw6

--- Lesson_6_HW5/test_1.py
+++ Lesson_6_HW5/test_1.py
@@ -5,6 +5,6 @@ from exercise1 import fizz_buzz
 class TestFizzBuzz(unittest.TestCase):
     """simple unit test checking None return"""
     def testing(self):
-        self.assertIsNotNone(fizz_buzz(1, 10), "should not be None")
+        self.assertIsNotNone(fizz_buzz(1, 100), "should not be None")
 
 
