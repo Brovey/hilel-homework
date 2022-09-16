eb6f29d2a807bbb031bd7378d83fcacba093e6ea
Sergii <s.o.petrov@gmail.com>
2022-09-16 23:15:20 +0300

    4 correction

--- Lesson_6_HW5/exercise4.py
+++ Lesson_6_HW5/exercise4.py
@@ -1,11 +1,9 @@
-import random
 from random import randint
 number = randint(1000000000000, 9999999999999)
 
 
 def get_max_digit(number):  # returns int
     """Using string and method sorted """
-    print(number)
     number_str = str(number)
     sorted_numbers = sorted(number_str, reverse=True)
     return int(sorted_numbers[0])
@@ -16,8 +14,7 @@ def get_max_digit_list(number):
     number_list = []
     while number != 0:
         number_list.insert(0, number % 10)  # used insert here to make  elements order  according to number
-        number = number//10
-    print(number_list)
+        number = number // 10
     max_digit = None
     for e in number_list:
         if max_digit is None or e > max_digit:
