4c33139eb7623c51a2ecb4f9f355c62df56fc5ee
Sergii <s.o.petrov@gmail.com>
2022-09-28 00:43:41 +0300

    updte

--- Lesson_8_HW6/excercise3.py
+++ Lesson_8_HW6/excercise3.py
@@ -1,10 +1,11 @@
-list1 = [5, '9', 0, '452', 6.5, '6', 1, 2]
-list2 = [472, 326, 1, '1101000', 9, '20', 863, '0']
+list1 = [5, '9', 0, '452', 6.5, '6', 1, 2, 99]
+list2 = [472, 326, 1, '1101000', 99,  '9', '20', 863, '0']
+
 
 
 def key(i):
-    i = str(i)
-    return i
+    elem = list(map(int, str(i)))
+    return elem[0]
 
 
 newlist1 = sorted(list1, key=lambda i: float(i))  # with lambda function
