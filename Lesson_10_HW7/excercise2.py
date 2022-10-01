cb0081f73b77f8cac1e77a049e493225ea00e4ca
Sergii <s.o.petrov@gmail.com>
2022-10-01 13:47:02 +0300

    1

--- Lesson_10_HW7/excercise2.py
+++ Lesson_10_HW7/excercise2.py
@@ -1,9 +1,9 @@
-def lchain(*iterables):  # returns list
-    elems = zip(*iterables)
+def lchain(*args):  # returns list
     new_list = []
-    for i in elems:
-        new_list.append(i)
+    for i in args:
+        for k in i:
+            new_list.append(k)
     return new_list
 
-result = lchain([1, 2, 3], {'5': 5}, tuple(), (6, 7), range(8, 10))
-print(result)
+
+print(lchain([1, 2, 3], {'5': 5}, tuple(), (6, 7), range(8, 10)))
