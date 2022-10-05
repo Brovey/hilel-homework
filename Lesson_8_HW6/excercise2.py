d51329378652c315cc1b7598667491c7378724a3
Sergii <s.o.petrov@gmail.com>
2022-10-05 14:18:53 +0300

    3

--- Lesson_8_HW6/excercise2.py
+++ Lesson_8_HW6/excercise2.py
@@ -1,14 +1,14 @@
 def copy_deep(lst):
-    list_new= []
-    for elem in lst:
-        if isinstance(elem, list):
-            list_new.append(copy_deep(elem))
-        else:
-            list_new.append(elem)
+    if isinstance(lst, list):
+        list_new = []
+        for elem in lst:
+            if isinstance(elem, list):
+                list_new.append(copy_deep(elem))
+            else:
+                list_new.append(elem)
     return list_new
-
 
 list1 = [1, 2, 3, ["a", "b", [1]]]
 list_new = copy_deep(list1)
-
+list_new[3][2] =[77777]
 print(list1, list_new)
