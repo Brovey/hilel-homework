33abb8c61cd1cd5a07aad43b6d9399b1d1251e22
Sergii <s.o.petrov@gmail.com>
2022-10-21 13:50:02 +0300

    HW10 correction

--- Lesson_14_HW10/excercise1.py
+++ Lesson_14_HW10/excercise1.py
@@ -1,13 +1,17 @@
 class Godzilla:
-    def __init__(self, stomach_volume_max=200):
+    def __init__(self, stomach_volume=0, stomach_volume_max=200):
         self.stomach_volume_max = stomach_volume_max
+        self.stomach_volume = stomach_volume
 
     def eating(self, inc_volume):
-        if (inc_volume / self.stomach_volume_max)*100 <= self.stomach_volume_max*0.9:
-            return "Godzilla can eat"
-        else:
-            return "Godzilla not hungry"
+        if self.stomach_volume + inc_volume <= self.stomach_volume_max*0.9:
+            self.stomach_volume += inc_volume
+            print(self.stomach_volume)
+        elif inc_volume > self.stomach_volume_max:
+            print(f"Godzilla cant eat more then {self.stomach_volume_max} kg")
+
+        elif self.stomach_volume >= self.stomach_volume_max*0.9:
+            print("Godzilla can't eat anymore")
 
 
 new_godzilla = Godzilla()
-print(new_godzilla.eating(90))
--- Lesson_14_HW10/excercise2.py
+++ Lesson_14_HW10/excercise2.py
@@ -9,10 +9,7 @@ class Circle:
 
     def check(self, inc_dot):
         segment_len = sqrt(((self.x - dot.x) ** 2) + ((self.y - dot.y) ** 2))
-        if segment_len <= self.radius:
-            return True
-        else:
-            return False
+        return segment_len <= self.radius
 
 
 class Dot:
