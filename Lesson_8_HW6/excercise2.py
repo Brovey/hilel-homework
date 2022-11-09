6baec9062f0f0ab0c2896ba7c124865b7cb5d581
Sergii <s.o.petrov@gmail.com>
2022-11-09 15:31:23 +0200

    hw6 update

--- Lesson_8_HW6/excercise2.py
+++ Lesson_8_HW6/excercise2.py
@@ -1,14 +1,17 @@
-def copy_deep(lst):
-    if isinstance(lst, list):
+def copy_deep(any_set):
+    if isinstance(any_set, tuple):
         list_new = []
-        for elem in lst:
-            if isinstance(elem, list):
-                list_new.append(copy_deep(elem))
-            else:
-                list_new.append(elem)
-    return list_new
-
-list1 = [1, 2, 3, ["a", "b", [1]]]
-list_new = copy_deep(list1)
-list_new[3][2] =[77777]
-print(list1, list_new)
+        for elem in any_set:
+            list_new.append(copy_deep(elem))
+        return tuple(list_new)
+    if isinstance(any_set, list):
+        list_new = []
+        for elem in any_set:
+            list_new.append(copy_deep(elem))
+        return list_new
+    if isinstance(any_set, dict):
+        dict_new = {}
+        for key, value in any_set.items():
+            dict_new[copy_deep(key)] = copy_deep(value)
+        return dict_new
+    return any_set
--- Lesson_8_HW6/excercise6.py
+++ Lesson_8_HW6/excercise6.py
@@ -6,28 +6,15 @@ def permutate(text):  # returns permuted text
     splitted_text = text.split()
     permuted_text = []
     final = []
-    #tmp_word = []
 
-    for elem in splitted_text:
-        shufled_words = []
-        if len(elem) > 3:
-            tmp_medium = elem[1:-1]
-            splitted_medium = [tmp_medium[i:i + 3] for i in range(0, len(tmp_medium), 3)]
-            #print(tmp_medium, splitted_medium)
-            for i in splitted_medium:
-                tmp_shufle = random.sample(i, len(i))
-                shufled_words.append(tmp_shufle)
-
-            shufled_words.insert(0, elem[0])  # adding 1st elem from word
-            shufled_words.append(elem[-1])    # adding last elem from word
-            print(tmp_medium, splitted_medium, shufled_words)
-
-            #shuf_medium = random.sample(i[1:-1], len(i[1:-1]))
-            #shuf_medium.insert(0, i[0])
-            #shuf_medium.append(i[-1])
-            #permuted_text.append(shuf_medium)
+    for i in splitted_text:
+        if len(i) > 3:
+            shuf_medium = random.sample(i[1:-1], len(i[1:-1]))
+            shuf_medium.insert(0, i[0])
+            shuf_medium.append(i[-1])
+            permuted_text.append(shuf_medium)
         else:
-            permuted_text.append(elem)
+            permuted_text.append(i)
     for i in permuted_text:
         word = "".join(i)
         final.append(word)
